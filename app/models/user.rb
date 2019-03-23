bef4f34c0c92235a13a2ed8d6c33b97147c28df9
doaakhalf <khalf_doaa@yahoo.com>
2019-03-23 20:09:44 +0200

    user_model

---
+++ app/models/user.rb
@@ -0,0 +1,33 @@
+# app/models/post.rb
+class User
+  #users : mostafa
+	#id,name,email,password,image,friends_id,group_id
+
+    # We define this class as a Mongoid Document
+    include Mongoid::Document
+  
+    # Generates created_at and updated_at
+    include Mongoid::Timestamps
+  
+    # Defining our fields with their types
+    field :name, type: String
+    field :email, type: String
+    field :password, type: String
+    field :image, type: String
+ 
+    # tags and comments will be stored inside the
+    # Post document
+    embeds_many :friends
+    embeds_many :groups
+  
+    # Sort the posts
+    scope :ordered, -> { order('created_at DESC') }
+  
+    # Validates that the slug is present and unique
+    validates :email, presence: true, uniqueness: true
+    validates :password, presence: true
+    validates :name, presence: true
+    # The slug has to be unique since it can be used to query post.
+    # Also defining an index will make the query more efficient
+    index({ slug: 1 }, { unique: true, name: "slug_index" })
+  end
