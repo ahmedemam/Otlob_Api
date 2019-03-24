582f020f6decb4813f0a2a6bea2b862c77ce618d
mostafa <mostapha.a.aly@gmail.com>
2019-03-24 12:35:16 +0200

    user controller

--- app/api/v1/users.rb
+++ app/api/v1/users.rb
@@ -1,14 +1,16 @@
-# app/api/v1/users.rb
+# app/api/v1/posts.rb
 module API
     module V1
       class Users < Grape::API
-        version 'v1', using: :path, vendor: 'otlob'
+        version 'v1', using: :path, vendor: 'samurails-blog'
+  
         resources :users do
+  
           desc 'Returns all users'
           get do
             User.all.ordered
           end
-
+  
           desc "Return a specific User"
           params do
             requires :id, type: String
@@ -16,6 +18,38 @@ module API
           get ':id' do
             User.find(params[:id])
           end
+  
+          desc "Login"
+          params do
+            requires :email, type: String
+            requires :password, type: String
+          end
+          post do
+            user = User.find(params[:email])
+            if user
+              { user }
+            else
+              get :status do
+                { status: 'user not exist' }
+              end
+            end
+          end
+
+          desc "Register"
+          params do
+            requires :email, type: String
+            requires :name, type: String
+            requires :password, type: String
+          end
+          post do
+            user = User.find(params[:email])
+            user.create!({
+              email: params[:email]
+              name: params[:name]
+              password: params[:password]
+            })
+          end
+          
         end
       end
     end
