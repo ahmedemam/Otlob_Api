868d3a22783ced1adc5800ff7178adc8a9d7db03
Ahmed Abd Elaziz <zizoahmed980@gmail.com>
2019-03-24 11:55:17 +0200

    Add Order Controller

--- app/api/v1/orders.rb
+++ app/api/v1/orders.rb
@@ -4,6 +4,7 @@ module API
     class Orders < Grape::API
       version 'v1', using: :path, vendor: 'samurails-blog'
       resources :orders do
+
         # get: /orders - return all orders
         desc 'Get All Orders'
         get do
@@ -20,7 +21,59 @@ module API
           requires :status, type: String
           requires :totalPrice, type: Integer
           requires :items, type: Array
+          requires :friends, type: Array
+          requires :groups, type: Array
+        end
+        post do
+          Order.create(
+              name: params[:name],
+              restaurantName: params[:restaurantName],
+              date: params[:date],
+              menuImage: params[:menuImage],
+              status: params[:status],
+              totalPrice: params[:totalPrice],
+              items: params[:items],
+              friends: params[:friends],
+              groups: params[:groups]
+          )
+        end
 
+        # delete: /orders/:id - delete order
+        desc 'Delete One Order'
+        params do
+          requries :id
+        end
+        delete ':id' do
+          Order.find(params[:id]).destroy
+        end
+
+        # update: /orders/:id
+        desc 'Update One Order'
+        params do
+          requires :id
+          requires :name, type: String
+          requires :date, type: DateTime
+          requires :restaurantName, type: String
+          requires :menuImage, type: String
+          requires :status, type: String
+          requires :totalPrice, type: Integer
+          requires :items, type: Array
+          requires :friends, type: Array
+          requires :groups, type: Array
+        end
+        put ':id' do
+          order = Order.find(params[:id])
+          order.update(
+              name: params[:name],
+              restaurantName: params[:restaurantName],
+              date: params[:date],
+              menuImage: params[:menuImage],
+              status: params[:status],
+              totalPrice: params[:totalPrice],
+              items: params[:items],
+              friends: params[:friends],
+              groups: params[:groups]
+          )
         end
       end
     end
@@ -29,7 +82,7 @@ end
 
 # Order Class / Collection
 # @name
-# @date,
+# @date
 # @restaurantName
 # @menuImage
 # @status
@@ -40,7 +93,9 @@ end
 
 
 # get:    /orders
+# get     /orders/:id  --
 # post:   /orders
 # delete: /orders/:id
 # put:    /orders/:id
-# get:    /orders/:id/lastest
+# get:    /orders/user/:id/latest
+#
